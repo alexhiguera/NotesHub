4742f02afdbe5a1b0d6830f16a7b52e658dff236
alexhiguera <alexhigueramartinez@gmail.com>
2024-02-01 13:49:16 +0100

    update: procesos en arbol añadido

---
+++ shell/procesos.sh
@@ -0,0 +1,20 @@
+# Procesos
+
+# Muestra los procesos que se están ejecutando en el sistema
+ps
+# Muestra los procesos que se están ejecutando en el sistema con más información
+ps -ef
+ps -aux
+# Formato árbol
+ps lf
+# Muestra los procesos que se están ejecutando en el sistema con más información y en formato árbol
+pstree
+
+# Listados de shokets (puertos)
+lsof
+lsof -i
+lsof -i :80
+
+# Visualizador de procesos
+top
+htop
